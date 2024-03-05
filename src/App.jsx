97772e93d163f5122f6b6d361ba77ddd9765a845
sergio chica <sergioanjaci@gmail.com>
2024-03-05 13:05:39 -0500

    Create Input

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+import { Main } from './components/Layouts/Main/Main'
+import { Cards } from './components/Cards/Cards'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Main>
+        <div className='containerInput'>
+            <input type="text" placeholder='Ingresa la tarea' className='inputCard' />
+            <button className='buttonCard'>Crear Tarea</button>
+        </div>
+        <div className="containerCards">
+            <Cards />
+        </div>
+      </Main>
+    </>
+  )
+}
+
+export default App
